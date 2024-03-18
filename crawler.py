594a7319abcf115f6a46eeb55be1bafd1d02c74d
maticklopcic <matic.klopcic314@gmail.com>
2024-03-18 18:34:45 +0100

    Preverjanje za binary datoteke.

--- crawler.py
+++ crawler.py
@@ -68,48 +68,87 @@ def url_exists(url_link):
     except requests.exceptions.RequestException:
         return False    
 
+def is_html(url):
+    try:
+        response = requests.head(url)
+        content_type = response.headers.get('Content-Type', '')
+        return 'text/html' in content_type
+    except Exception as e:
+        print("Error:", e)
+        return False
+
+def is_binary(url):
+    parts = url.split("/")
+    filename = parts[-1]
+    extension_parts = filename.split(".")
+    if len(extension_parts) > 1:
+        return "." + extension_parts[-1]
+    else:
+        return ""
+    
+def ends_with_extension(url):
+    document_extensions = ['.pdf', '.doc', '.docx', '.ppt', '.pptx']
+    for extension in document_extensions:
+        if url.endswith(extension):
+            return True, extension
+    return False
+
+def get_url_extension(url):
+    parsed_url = urlparse(url)
+    path = parsed_url.path
+    extension_parts = path.split(".")
+    if len(extension_parts) > 1:
+        return "." + extension_parts[-1]
+    else:
+        return ""
     
 def get_html_and_links(frontier):
     with webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options) as driver:
         i = 0
         while not frontier.empty():
             web_address = frontier.get()
-            print(f"Retrieving web page URL '{web_address}'")
+            print(f"{i}Retrieving web page URL '{web_address}'")
 
             robots_url = web_address + "robots.txt"
-            if is_allowed(web_address, "*", robots_url):
-                crawl_delay = get_crawl_delay(robots_url)
-                wait = WebDriverWait(driver, crawl_delay)
-                try:
-                    driver.get(web_address)
-                except Exception as e:
-                    print(f"An exception occurred: {e}") 
+            #if is_allowed(web_address, "*", robots_url):
+            crawl_delay = get_crawl_delay(robots_url)
+            wait = WebDriverWait(driver, crawl_delay)
+            try:
+                driver.get(web_address)
+            except Exception as e:
+                print(f"An exception occurred: {e}") 
 
-                html = driver.page_source
-                links = driver.find_elements(By.TAG_NAME, "a")
+            html = driver.page_source
+            links = driver.find_elements(By.TAG_NAME, "a")
 
-                for link in links:
-                    href = link.get_attribute("href")
-                    
-                    start_time = time.time()
-                    
-                    #print(f"{robots_url}  {time.time() - start_time} seconds ---, ------>>>>> , ??? {exi}")
+            for link in links:
+                href = link.get_attribute("href")
+                
+                start_time = time.time()
+                
+                #print(f"{robots_url}  {time.time() - start_time} seconds ---, ------>>>>> , ??? {exi}")
 
-                    if href is not None and href[0:4] == "http":
-                        href = remove_query_and_fragment(href)
-                        robots_url = get_robots_url(href)
-                        if href in added_urls_set:                  #checks if url was/is in frontier
+                if href is not None and href[0:4] == "http":
+                    if is_binary(web_address):
+                        extension = get_url_extension(web_address)
+                        if extension in ['.pdf', '.doc', '.docx', '.ppt', '.pptx']:
+                            print("Binary file: ", extension, "web: ", web_address)
+                            #dont read it, just save it as binary with extension
                             continue
-                        if url_exists(get_domain(robots_url)):
-                            allowance = is_allowed(href, "*", robots_url)
-                        else:
-                            allowance = True
-                        if allowance:
-                            frontier.put(href)
-                            added_urls_set.add(href)
+                    href = remove_query_and_fragment(href)
+                    robots_url = get_robots_url(href)
+                    if href in added_urls_set:                  #checks if url was/is in frontier
+                        continue
+                    if url_exists(get_domain(robots_url)):
+                        allowance = is_allowed(href, "*", robots_url)
+                    else:
+                        allowance = True
+                    if allowance:
+                        frontier.put(href)
+                        added_urls_set.add(href)
             
             html_hash.write(str(hash(html)) + '\n')
-            if i == 10:
+            if i == 100:
                 for item in added_urls_set:
                     urls_file.write(str(item) + "\n")
                 break
@@ -140,7 +179,7 @@ def remove_query_and_fragment(url):
 
 
 get_html_and_links(frontier)
-print_frontier(frontier)
+#print_frontier(frontier)
 html_hash.close()
 urls_file.close()
 
