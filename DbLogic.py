8b4639d868cba88ad3e28934479761acf16ff19b
JureArsovic <ja6533@student.uni-lj.si>
2024-03-21 10:16:33 +0100

    save_site updated z sitemapom

--- DbLogic.py
+++ DbLogic.py
@@ -55,6 +55,9 @@ class DbLogic:
             finally:
                 conn.close()
 
+    def insert_image():
+        return
+
 
     def check_page_exists(self, url):
         conn = self.connect_to_db()
@@ -88,22 +91,6 @@ class DbLogic:
             finally:
                 conn.close()
 
-    def save_page(self, site_id, url, html_content, page_hash, http_status_code, accessed_time, page_type_code):
-        conn = self.connect_to_db()
-        if conn is not None:
-            try:
-                with conn.cursor() as cur:
-                    cur.execute("""
-                        INSERT INTO crawldb.page (site_id, url, html_content, hash_value, http_status_code, accessed_time, page_type_code)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)
-                        ON CONFLICT (url) DO NOTHING;
-                    """, (site_id, url, html_content, page_hash, http_status_code, accessed_time, page_type_code))
-                    conn.commit()
-                    #print(f"Saved URL: {url}, HASH: {page_hash}, HTML CONTENT: {html_content} ACCESSED TIME: {accessed_time} to the database")
-            except Exception as e:
-                print(f"Error saving page {url}: {e}")
-            finally:
-                conn.close()
 
     def save_page_frontier(self, url, http_status_code, accessed_time):
         conn = self.connect_to_db()
@@ -202,16 +189,16 @@ class DbLogic:
                 conn.close()
 
 
-    def save_site(self, domain, robots_content):
+    def save_site(self, domain, robots_content, sitemap_content):
         conn = self.connect_to_db()
         if conn is not None:
             try:
                 with conn.cursor() as cur:
                     cur.execute("""
-                        INSERT INTO crawldb.site (domain, robots_content)
-                        VALUES (%s, %s)
+                        INSERT INTO crawldb.site (domain, robots_content, sitemap_content)
+                        VALUES (%s, %s, %s)
                         RETURNING id;
-                    """, (domain, robots_content))
+                    """, (domain, robots_content, sitemap_content))
                     site_id = cur.fetchone()[0]
                     conn.commit()
                     print(f"Site with domain {domain} and ID {site_id} has been saved to the database.")
