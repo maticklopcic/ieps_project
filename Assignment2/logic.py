e1b56717d53b4e25525505f18a74be6e63ac1438
JureArsovic <ja6533@student.uni-lj.si>
2024-04-10 22:55:56 +0200

    HTMLji zrenderani

--- Assignment2/logic.py
+++ Assignment2/logic.py
@@ -1,11 +1,38 @@
 import sys
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from pathlib import Path
 from Regex import Regex
 from Xpath import Xpath
 from RoadRunner import RoadRunner
 
+paths = [
+    Path('strani', 'rtvslo.si', 'Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'),
+    Path('strani', 'rtvslo.si', 'Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html'),
+    Path('strani', 'overstock.com', 'jewelry01.html'),
+    Path('strani', 'overstock.com', 'jewelry02.html')
+]
+
+options = FirefoxOptions()
+#options.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service, options=options)
+
+html_contents = []
+
+for path in paths:
+    file_uri = path.resolve().as_uri()
+    driver.get(file_uri)
+    html_contents.append(driver.page_source)
+
+driver.quit()
+
 def extract_using_regex():
     regex = Regex()
-    regex.rtv()
+    regex.rtv(html_contents[0], html_contents[1])
+    regex.overstock(html_contents[2], html_contents[3])
     print("Extracting using regex...")
     return []
 
@@ -23,11 +50,10 @@ def extract_using_road_runner():
 def main():
     if len(sys.argv) != 2:
         print("Usage: python logic.py [method]")
-        print("method: regex, xpath, or road_runner")
+        print("method: 'A', 'B', 'C'")
         sys.exit(1)
 
     method = sys.argv[1]
-    html_content = "<html>...</html>"
 
     if method == 'A':
         result = extract_using_regex()
