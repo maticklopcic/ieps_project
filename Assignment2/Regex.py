9a73bca517a6cb32dda59a85a769d517ff6caa7b
JureArsovic <ja6533@student.uni-lj.si>
2024-04-10 23:24:37 +0200

    titles, prices, listed_prices extracted

--- Assignment2/Regex.py
+++ Assignment2/Regex.py
@@ -6,23 +6,23 @@ class Regex:
     def __init__(self) -> None:
         pass
 
-    def rtv(self):
+    def rtv(self, rtv1_html_content, rtv2_html_content):
         # Example regex patterns
         title_pattern = r'<h1[^>]*>(.*?)</h1>'
-        item_pattern = r'<li[^>]*>(.*?)</li>'  # Assuming items are in list elements
-        price_pattern = r'\$\d+\.\d+'  # Matches prices like $19.99
+        item_pattern = r'<li[^>]*>(.*?)</li>'
+        price_pattern = r'\$\d+\.\d+'
 
         # Extract titles
-        titles1 = re.findall(title_pattern, self.rtv1_html_content)
-        titles2 = re.findall(title_pattern, self.rtv2_html_content)
+        titles1 = re.findall(title_pattern, rtv1_html_content)
+        titles2 = re.findall(title_pattern, rtv2_html_content)
 
         # Extract item lists (assuming they are within <li> tags)
-        items1 = re.findall(item_pattern, self.rtv1_html_content)
-        items2 = re.findall(item_pattern, self.rtv2_html_content)
+        items1 = re.findall(item_pattern, rtv1_html_content)
+        items2 = re.findall(item_pattern, rtv2_html_content)
 
         # Extract prices
-        prices1 = re.findall(price_pattern, self.rtv1_html_content)
-        prices2 = re.findall(price_pattern, self.rtv2_html_content)
+        prices1 = re.findall(price_pattern, rtv1_html_content)
+        prices2 = re.findall(price_pattern, rtv2_html_content)
 
         extracted_info = {
             "titles": {"rtv1": titles1, "rtv2": titles2},
@@ -32,9 +32,27 @@ class Regex:
         print(extracted_info)
         return extracted_info
     
-    def overstock(self):
-        print(self.ovs1_html_content)
-        return "regex"
+    def overstock(self, ovs1_html_content, ovs2_html_content):
+        title_pattern = r'<a href="http://www.overstock.com/cgi-bin/d2.cgi\?PAGE=PROFRAME&amp;PROD_ID=\d+"><b>([^<]+)</b></a><br>'
+        list_prices_pattern = r'<s>([^<]+)</s>'
+        price_pattern = r'<tr><td align="right" nowrap="nowrap"><b>Price:</b></td><td align="left" nowrap="nowrap"><span class="bigred"><b>'
+
+        titles1 = re.findall(title_pattern, ovs1_html_content)
+        titles2 = re.findall(title_pattern, ovs2_html_content)
+        
+        list_prices1 = re.findall(list_prices_pattern, ovs1_html_content)
+        list_prices2 = re.findall(list_prices_pattern, ovs2_html_content)
+
+        prices1 = re.findall(price_pattern, ovs1_html_content)
+        prices2 = re.findall(price_pattern, ovs2_html_content)
+
+        extracted_info = {
+            "titles": {"OVS1": titles1, "OVS2": titles2},
+            "list_prices": {"OVS1": list_prices1, "OVS2": list_prices2},
+            "prices": {"OVS1": prices1, "OVS2": prices2}
+        }
+        print(extracted_info)
+        return extracted_info
     
     def custom(self, html):
         return "regex"
