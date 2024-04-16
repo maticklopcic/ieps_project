d6bd03281e526c905c6f3fb98769722d7e98ae6f
JureArsovic <ja6533@student.uni-lj.si>
2024-04-16 15:20:03 +0200

    custom pages rendered

--- Assignment2/run-extraction.py
+++ Assignment2/run-extraction.py
@@ -8,26 +8,41 @@ from webdriver_manager.firefox import GeckoDriverManager
 from Regex import Regex
 from Xpath import Xpath
 from RoadRunner import RoadRunner
-"""
+
 paths = [
-    Path('strani', 'rtvslo.si', 'Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'),
-    Path('strani', 'rtvslo.si', 'Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html'),
-    Path('strani', 'overstock.com', 'jewelry01.html'),
-    Path('strani', 'overstock.com', 'jewelry02.html')
+    #Path('strani', 'rtvslo.si', 'Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'),
+    #Path('strani', 'rtvslo.si', 'Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html'),
+    #Path('strani', 'overstock.com', 'jewelry01.html'),
+    #Path('strani', 'overstock.com', 'jewelry02.html')
+    "https://www.rottentomatoes.com/browse/movies_at_home/affiliates:peacock",
+    "https://www.rottentomatoes.com/browse/movies_at_home/affiliates:amazon_prime"
 ]
 
 options = FirefoxOptions()
 options.add_argument("--headless")
 service = Service(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service, options=options)
+#html_contents = []
+"""
+CUSTOM
+rendered_file_path = os.path.join('strani', 'rendered_custom.txt')
+os.makedirs(os.path.dirname(rendered_file_path), exist_ok=True)
+
+
+with open(rendered_file_path, 'w', encoding='utf-8') as file:
+    for path in paths:
+        driver.get(path)
+        page_source = driver.page_source
+        print(page_source)
+        file.write(page_source + "\n\n--- End of HTML Content ---\n\n")
+
+driver.quit()
+"""
+"""
+RTV IN OVERSTOCK
 rendered_file_path = os.path.join('strani', 'rendered.txt')
 os.makedirs(os.path.dirname(rendered_file_path), exist_ok=True)
 
-html_contents = []
-
-for path in paths:
-    file_uri = path.resolve().as_uri()
-    driver.get(file_uri)
 
 with open(rendered_file_path, 'w', encoding='utf-8') as file:
     for path in paths:
@@ -50,6 +65,21 @@ if os.path.exists(file_path):
 else:
     print("File not found:", file_path)
 
+file_path2 = os.path.join('strani', 'rendered_custom.txt')
+
+if os.path.exists(file_path2):
+    with open(file_path2, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    for section in content.split("\n\n--- End of HTML Content ---\n\n"):
+        if section.strip():
+            html_contents.append(section.strip())
+
+    print("Number of HTML contents loaded:", len(html_contents))
+else:
+    print("File not found:", file_path2)
+
+print("DOLZINA HTMLJEV: ", len(html_contents))
 def extract_using_regex():
     regex = Regex()
     regex.rtv(html_contents[0], html_contents[1])
