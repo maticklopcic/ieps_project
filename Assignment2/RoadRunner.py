1a7aad01b450d73f3f8085dc0c13eac4b3a50666
KG9414 <72404881+KG9414@users.noreply.github.com>
2024-04-18 07:22:52 +0200

    popravljen RoadRunner

--- Assignment2/RoadRunner.py
+++ Assignment2/RoadRunner.py
@@ -1,10 +1,9 @@
 import json
-import re
-import copy
 from bs4 import BeautifulSoup, NavigableString, Comment, Tag
 class RoadRunner:
     def __init__(self):
-        pass
+        self.ignore_tags = ["b", "script", "br", "em", "hr"]
+        self.self_closing_tags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"]
 
     #def test_method(self):
     #   print("Test method called successfully.")
@@ -12,8 +11,8 @@ class RoadRunner:
     def extract_data(self, html1, html2):
         soup1 = self.prepare_soup(html1)
         soup2 = self.prepare_soup(html2)
-        wrapper = self.tokenize_and_compare(str(soup1), str(soup2))
-        
+        wrapper = self.road_runner(soup1, soup2)
+
         extracted_data = {
             #'html1': str(soup1),
             #'html2': str(soup2),
@@ -26,18 +25,20 @@ class RoadRunner:
     def prepare_soup(self, html_content):
         soup = BeautifulSoup(html_content, 'html.parser')
         self.decompose_unwanted(soup)
-        self.clean_soup(soup)
+        #self.clean_soup(soup)
 
         return soup
     
     def decompose_unwanted(self, soup):
-        for element in soup.find_all(['script', 'style', 'meta']):
-            element.decompose()
+        for tag in  self.ignore_tags + ['script', 'style', 'meta']:
+            for element in soup.find_all(tag):
+                element.decompose()
 
         comments = soup.findAll(string=lambda text: isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
 
-        [comment.extract() for comment in comments]
-
+    """
     def clean_soup(self, soup):
         stack = [soup]
         while stack:
@@ -51,32 +52,30 @@ class RoadRunner:
                     current.replace_with(current.string.strip())
             elif current:
                 stack.extend(list(current.children))
+    """
 
     def tokenize_and_compare(self, html1, html2):
         tokens1 = self.tokenize(html1)
         tokens2 = self.tokenize(html2)
 
-        wrapper = self.road_runner(tokens1, tokens2)
-        return wrapper
+        return self.road_runner(tokens1, tokens2)
 
     def tokenize(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         tokens = []
         
         for element in soup.recursiveChildGenerator():
-            if isinstance(element, str):
-                if element.strip():
-                    tokens.append(element.strip())
-            else:
-                if element.name:
-                    #Capture the tag with its attributes
-                    #attrs = ''.join([f' {k}="{v}"' for k, v in element.attrs.items()])
-                    #tokens.append(f"<{element.name}{attrs}>")
-                    tokens.append(f"<{element.name}>")
-                    if element.contents:
-                        tokens.append(f"</{element.name}>")
+            if isinstance(element, Tag):
+                open_tag = f"<{element.name}>"
+                tokens.append(open_tag)
+                if element.string:
+                    tokens.append(element.string.strip())
+                tokens.append(f"</{element.name}>")
+                if element.name not in self.self_closing_tags:
+                    tokens.append(f"</{element.name}>")
         return tokens
-    
+
+    """
     def match_structures(self, soup1, soup2):
         if soup1.name != soup2.name:
             return False
@@ -86,35 +85,46 @@ class RoadRunner:
                     s1.replace_with(self.generalize_strings(str(s1), str(s2)))
             elif s1.name == s2.name:
                 self.match_structures(s1, s2)
+    """
+    def check_repeating(self, tokens, index):
+        start_tag = tokens[index]
+        count = 1
+        i = index + 1
+        while i < len(tokens) and count > 0:
+            if tokens[i] == start_tag:
+                count += 1
+            elif tokens[i] == f"</{start_tag[1:]}":
+                count -= 1
+            i += 1
+        return tokens[index:i], i
     
+    def format_repeating(self, tokens):
+        return f"({tokens[0]} ... {tokens[-1]})?"
+
+    def merge_repeating(self, repeat1, repeat2):
+        return f"Merge({repeat1[0]} ... {repeat1[-1]}, {repeat2[0]} ... {repeat2[-1]})?"
     
     def road_runner(self, tokens1, tokens2):
-        index1 = 0
-        index2 = 0
-        wrapper = []
-
-        while index1 < len(tokens1) and index2 < len(tokens2):
-            if tokens1[index1] == tokens2[index2]:
-                wrapper.append(tokens1[index1])
-                index1 += 1
-                index2 += 1
-            else:
-                # Check if skipping a token in tokens1 aligns them
-                if index1 + 1 < len(tokens1) and tokens1[index1 + 1] == tokens2[index2]:
-                    index1 += 1
-                # Check if skipping a token in tokens2 aligns them
-                elif index2 + 1 < len(tokens2) and tokens1[index1] == tokens2[index2 + 1]:
-                    index2 += 1
+        if tokens1.name != tokens2.name:
+            return f"<{tokens1.name}> #PCDATA </{tokens1.name}>"
+        result = [f"<{tokens1.name}>"]
+        if tokens1.name not in self.self_closing_tags and tokens1.name not in self.ignore_tags:
+            children1 = list(tokens1.children)
+            children2 = list(tokens2.children)
+            i, j = 0, 0
+            while i < len(children1) and j < len(children2):
+                if isinstance(children1[i], NavigableString) or isinstance(children2[j], NavigableString):
+                    if str(children1[i]).strip() != str(children2[j]).strip():
+                        result.append("#PCDATA")
+                    else:
+                        result.append(str(children1[i]).strip())
+                    i += 1
+                    j += 1
                 else:
-                    # If no direct or single-skip alignment is found, skip both
-                    index1 += 1
-                    index2 += 1
-
-        return ' '.join(wrapper)
+                    result.append(self.road_runner(children1[i], children2[j]))
+                    i += 1
+                    j += 1
+            result.append(f"</{tokens1.name}>")
+        return ' '.join(result)
     
-    def generalize_strings(self, str1, str2):
-       
-        if str1 != str2:
-            return ".*"
-        return str1
     
